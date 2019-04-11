b301954582c79db4388a36dd7423a8b98e6cee30
Nguyen Kim Hiep <nkhiep@tma.com.vn>
2019-04-11 14:45:25 +0700

    :+1:

--- script2.sh
+++ script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "				    1. Add SSH "
+echo "                  1. Add SSH "
 mkdir -p /root/.ssh/
 cat << EOF > /root/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDANXW1NsDxlnQgbjvj6YmL1xfUXvX/ygnXb2XXOVfPuUGIPSJh18TEb+ie5AZJ707AmnICZmQlrxJOCTI8R5mwggRZ+oO5qZtRohf54hrmfJdHRkQbN7F7LEyr8QQ3FY7at7fmaH6xrxDtQdZnDnpXybMQEw29S9cOFEHQErE/qpkO5N/u8vp0VkwHteSgCSMcCsXhX+3CDgQ15Ooo3gMnh8Z6dqPOU+wIRELqZDrlwZ7q3Jy1YNJL0Yit8OIIv7TQtCPUx/J3w2VPnJv7pR+cBhzDMRIz6rqBhbgC+OsqoDdam8oT2rAMZ4+4b4OWywSaF2IKiGWBLLZBepd0c3vx imported-openssh-key
